078f65992bd773767e2097bd39e13d95dd00e681
Simeon <simtbak@users.noreply.github.com>
2022-02-13 16:30:06 +0000

    Overwrite limiting scroll events

--- popup.js
+++ popup.js
@@ -9,4 +9,7 @@ document.addEventListener("mousemove", async () => {
 
 function unhideScroll(e) {
     document.getElementsByTagName('body')[0].style = "overflow-y:scroll";
+	window.onscroll = function () { console.log('scrolling'); }
+	document.onscroll = function () { console.log('scrolling'); }
+	document.body.onscroll = function () { console.log('scrolling'); }
 }
